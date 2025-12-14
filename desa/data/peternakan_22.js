0b788a87ada383517dd2c2eede520252b003a96b
fatma adysalina <fatmadys2@gmail.com>
2025-12-15 03:51:58 +0700

    first commit

---
+++ desa/data/peternakan_22.js
@@ -0,0 +1,1 @@
+var json_peternakan_22 = {"type":"FeatureCollection","name":"peternakan_22","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","nama":"peternakan pak mardiono","alamat":"kebocoran"},"geometry":{"type":"Point","coordinates":[109.210075270739495,-7.396261541825555]}}]}
---
+++ kecamatan/data/rumah_24.js
@@ -0,0 +1,1 @@
+var json_rumah_24 = {"type":"FeatureCollection","name":"rumah_24","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[109.204666048678803,-7.398864637529694]}}]}
